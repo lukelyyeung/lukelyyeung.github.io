bcb61c37c54007d67c81705b3626d9f7a99c8663
lukelyyeung <lukelyyeung@DESKTOP-QSFNC0C.localdomain>
2017-12-31 21:31:08 +0800

    mobile touch test added

---
+++ Personal/assets/js/touch.js
@@ -0,0 +1,33 @@
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if (! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( yDiff ) > Math.abs( xDiff ) ) {
+        if ( yDiff > 0 ) {
+            $("#carouselSPAControls").carousel("prev"); 
+        } else { 
+            $("#carouselSPAControls").carousel("next"); 
+        }                                                                 
+    }
+
+    xDown = null;
+    yDown = null;                                             
+};
